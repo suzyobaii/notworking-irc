08e094b2582755b29cd26956ef04114a23282657
mumomuthike <mumo.muthike@gmail.com>
2025-11-28 14:58:51 -0500

    final changes

--- Scripts/log_setup.py
+++ Scripts/log_setup.py
@@ -1,20 +1,16 @@
 import logging  #importing the built in python logging 
 
 def setupLogClient():
-    '''
-    Setting up the logger for the client server
-    '''
-    # Logging for client
-    def setupLogClient():
-        logging.basicConfig(
-            filename="client.log",
-            level=logging.INFO,
-            format="%(asctime)s - %(levelname)s - %(message)s"
-        )
+    logging.basicConfig(
+    filename="client.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+    )
 # Logging for server
-    def setupLogServer():
-        logging.basicConfig(
-            filename="server.log",
-            level=logging.INFO,
-            format="%(asctime)s - %(levelname)s - %(message)s"
-        )
+
+def setupLogServer():
+    logging.basicConfig(
+    filename="server.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+    )
